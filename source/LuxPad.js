a8e7e2dcef903c645681c2595bdd47b1a69f854b
L1lith <lily@l1lith.com>
2019-01-25 12:25:26 -0700

    fix search

--- source/LuxPad.js
+++ source/LuxPad.js
@@ -35,7 +35,7 @@ class LuxPad {
     const queries = Object.entries(search)
     return this.controllers.filter(controller => {
       if (!controller) return false
-      return queries.filter((property, value) => {
+      return queries.every((property, value) => {
         controller[property] === value
       })
     })
