3d2f1239e5177be8ff8c4ed922ceea1e5e510f9b
L1lith <lily@l1lith.com>
2019-01-25 12:40:49 -0700

    fix reference issue

--- source/controllers/functions/standardAxes.js
+++ source/controllers/functions/standardAxes.js
@@ -1,10 +1,10 @@
 function standardAxes(luxController, axes1, axes2, deadZone = 0.1) {
   return ()=>{
     const {rawController} = luxController
-    let x = rawControllers.axes[axes1]
-    let y = rawControllers.axes[axes2]
-    if (Math.abs(axes1) <= deadZone) x = 0
-    if (Math.abs(axes2) <= deadZone) y = 0
+    let x = rawController.axes[axes1]
+    let y = rawController.axes[axes2]
+    if (Math.abs(x) <= deadZone) x = 0
+    if (Math.abs(y) <= deadZone) y = 0
     return {x, y}
   }
 }
