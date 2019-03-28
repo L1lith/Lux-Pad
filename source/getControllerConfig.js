d573852c7216584a5154e8edc5c9042e5a029a9f
L1lith <lily@l1lith.com>
2019-03-28 17:08:34 -0600

    support adding custom controller definitions on the fly

--- source/getControllerConfig.js
+++ source/getControllerConfig.js
@@ -3,6 +3,13 @@ import Xbox360Controller from "./controllers/360"
 import fightpadpro from './controllers/fightpadpro'
 const controllers = [mayflash, Xbox360Controller, fightpadpro]
 
+export function addControllerDefinition(definition) {
+	if (typeof definition != 'object' || definition === null) throw new Error("Controller definition must be an object")
+	if (typeof definition.type != 'string' || definition.type.length < 1) throw new Error("Controller definition type must be a string")
+	if (Object.keys(definition).length < 2) throw new Error("No options were provided in the controller definition.")
+	if (!controllers.includes(definition)) controllers.append(definition)
+}
+
 function getControllerConfig(controller) {
 	for (let i = 0; i < controllers.length; i++) {
 		const checkController = controllers[i]
--- source/index.js
+++ source/index.js
@@ -1,4 +1,5 @@
 export { default as LuxPad } from "./LuxPad"
 export { default as ControllerMapping } from "./ControllerMapping"
+export { addControllerDefinition as addControllerDefinition } from "./getControllerConfig"
 
 //export default LuxPad
